d4fa53058a05ef4cf30c7efeaecdc4a65646e0d9
Muhammed Çağrı Kurt <54119447+muhammedcagrikurt@users.noreply.github.com>
2023-01-05 13:10:21 +0000

    -

--- src/App.js
+++ src/App.js
@@ -1,75 +1,12 @@
 import './App.css';
-import React, {useState} from "react";
+import Form from "./Components/Form"
 
 function App() {
-  const [score,setScore]= useState(0);
-  const [scores,setScores]=useState([]);
- const [name,setName]=useState("");
-  const [names,setNames]=useState([]);
-   //const [laptop,setLaptop]=useState([{names:"",scores:""}])
-   const addLaptop=()=>{
-     setScores([...scores,score]);
-     setNames([...names,name]);
-     //setLaptop({...laptop,names:name,scores:score})
-     setName("");
-     setScore(0);
-   }
- 
-     return(
-      <div >
-       
-         <h2>İşlemci Seçiniz</h2>
-               <button onClick={()=>setScore(score+ 1 )}>i5</button>
-               <button onClick={()=>setScore(score+ 2 )}>i7</button>
-               
-             <hr/>
-             <h2>İşlemci Nesili Seçiniz</h2>
-             <button onClick={()=>setScore(score+ 2 )}>11.Nesil</button>
-             <button onClick={()=>setScore(score+ 1 )}>12.Nesil</button>
- 
-             <hr/>
-             <h2>Ekran Kartı Seçiniz</h2>
-       
-         <button onClick={()=>setScore(score+ 1 )}>1650</button>
-         <button onClick={()=>setScore(score+ 2 )}>1650 Ti</button>
-         <button onClick={()=>setScore(score+ 3 )}>3050</button>
-         <button onClick={()=>setScore(score+ 4 )}>3050 Ti</button>
-         <button onClick={()=>setScore(score+ 5 )}>3060</button>
-        
-         <hr/>
-         <h2>Ram Seçiniz</h2>
- 
-         <button onClick={()=>setScore(score+ 1 )}>8 Gb</button>
-         <button onClick={()=>setScore(score+ 2 )}>16 Gb</button>
- 
-         <hr/>
-         <h2>Güvenilirlik Durumu Seçiniz</h2>
-         <button onClick={()=>setScore(score+ 1 )}>Evet</button>
-         <button onClick={()=>setScore(score+ -1 )}>Hayır</button>
- 
-         <hr/>
-         <h2>Fiyat</h2>
-         <hr/>
-         {console.log(score)}
-         <input type="text" name="name" value={name} placeholder="Laptop Markası/İsmi" onChange={(e)=>{setName(e.target.value)}}/>
-         <button onClick={addLaptop}>Kaydet</button>
- 
-       <ul> 
-         <ol>
-       {names.map((names,index) => (
-           <li key={index}>{names}</li>
-         ))}</ol>
-          <ol>
-       {scores.map((scores,index) => (
-            <li key={index}>{scores}</li>
-          ))}</ol>
- 
-  
-         </ul>
-       
-      </div>
-     );
- };
-
+  return (
+    <div >
+      <Form/>
+    </div>
+  );
+}
 
 export default App;
